687e6b163e00bd59db080353bde80eda542e5902
James Gillispie <james.gillispie@gmail.com>
2025-09-07 18:26:34 -0400

    Connect pages to backend and add basic blocks.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use A17\Twill\Facades\TwillNavigation;
+use A17\Twill\View\Components\Navigation\NavigationLink;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +19,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
         //
+        TwillNavigation::addLink(
+            NavigationLink::make()->forModule('pages')
+        );
     }
 }
