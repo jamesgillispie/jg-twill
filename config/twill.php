9a719c5a7848f93214f554fdab9ca02befd0a10e
James Gillispie <james.gillispie@gmail.com>
2025-09-07 20:52:25 -0400

    Add tailwind.

--- config/twill.php
+++ config/twill.php
@@ -3,6 +3,22 @@
 return [
     'block_editor' => [
         'use_twill_blocks' => [],
+        'crops' => [ 
+            'highlight' => [
+                'desktop' => [
+                    [
+                        'name' => 'desktop',
+                        'ratio' => 16 / 9,
+                    ],
+                ],
+                'mobile' => [
+                    [
+                        'name' => 'mobile',
+                        'ratio' => 1,
+                    ],
+                ],
+            ],
+        ],
         'block_single_layout' => 'site.layouts.block',
     ]
 ];
